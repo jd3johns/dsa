7db8cdd35e75d343f1e1df88ae6952dd0d56c733
jd3johns <jd3johns@uwaterloo.ca>
2015-08-26 15:03:43 -0400

    Updated stack and queue implementations such that they appropriately use pointers as instance attributes rather than class attributes.

--- python/queues/queue.py
+++ python/queues/queue.py
@@ -7,9 +7,13 @@ class Queue(object):
     '''
     Queue data structure for generic data using linked list nodes.
     '''
-    first, last = None, None
+    def __init__(self):
+        ''' Initialize the queue with null references to first and last '''
+        self.first = None
+        self.last = None
 
     def enqueue(self, data):
+        ''' Append a new node to the back of the line '''
         if self.last is None:
             self.last = node.Node(data)
             self.first = self.last
@@ -18,6 +22,7 @@ class Queue(object):
             self.last = self.last.next
 
     def dequeue(self):
+        ''' Pull a node off of the front of the line, returning its value '''
         if self.first is not None:
             element = self.first.data
             self.first = self.first.next
--- python/stacks/stack.py
+++ python/stacks/stack.py
@@ -7,14 +7,18 @@ class Stack(object):
     '''
     Stack data structure for generic data using linked list nodes.
     '''
-    top = None
+    def __init__(self):
+        ''' Initialize the stack as being a null pointer to the top '''
+        self.top = None
 
     def push(self, data):
+        ''' Push a node on top of the stack '''
         new_top = node.Node(data)
         new_top.next = self.top
         self.top = new_top
 
     def pop(self):
+        ''' Pop the top node off of the stack and return its value '''
         if self.top is not None:
             element = self.top.data
             self.top = self.top.next
@@ -23,6 +27,7 @@ class Stack(object):
             return None
 
     def peek(self):
+        ''' Return the value of the top node of the stack '''
         if self.top is not None:
             return self.top.data
         else:
